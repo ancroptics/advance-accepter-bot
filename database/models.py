c5884c1421ecdda544a9bf94515d54cbe5f5c60c
ancroptics <ancroptics@gmail.com>
2026-04-05 22:12:31 +0530

    Update database/models.py with full deployed version

--- database/models.py
+++ database/models.py
@@ -8,5 +8,3 @@ logger = logging.getLogger(__name__)
 MIGRATION_SQL_PATH = os.path.join(os.path.dirname(__file__), 'migrations', '001_initial_schema.sql')
 
 class DatabaseModels:
-    def __init__(self, pool):
-        self.db = pool
